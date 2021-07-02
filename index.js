55f15e28a3c581dee3e337dba84c34ffcec5cbb9
Emmanuel <flotuz10@gmail.com>
2021-07-02 11:55:44 -0500

    Estamos en llamas

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/usuarios", (req, res) => {
+app.get("/", (req, res) => {
   res.send("API THE CORE");
 });
 
